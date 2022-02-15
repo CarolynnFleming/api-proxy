f71f665ffac896db332f784b5d2f1ef4e877a52e
CarolynnFleming <cfcatpink@gmail.com>
2022-02-14 19:02:15 -0500

    submit

--- src/PokemonSearch.js
+++ src/PokemonSearch.js
@@ -2,29 +2,31 @@ import { useState } from 'react';
 
 export default function PokemonSearch() {
       // you'll need to track your pokemon search results, the loading state, and one form field: name. For this form field, set a real initial values (like 'pikachu') so the form populates with a default value.
+  const [loading, setLoading] = useState(false);
   const [pokemon, setPokemon] = useState([]);
-  const [search, setSearch] = useState('');
-  const [searchBy, setSearchBy] = useState('pokemon');
+  const [search, setSearch] = useState('bulbasaur');
 
   async function handlePokemonSubmit(e) {
     e.preventDefault();
       
         // set the loading state to true
+    setLoading(true);
         // use fetch to make a request to your netlify pokemon function. Be sure to pass the pokemon name as a query param in the URL
-    const endpointURL = `https://pokedex-alchemy.herokuapp.com/api/pokedex?${searchBy}=${search}`;
-
-    const response = await fetch(endpointURL);
+    const response = await fetch(`/.netlify/functions/pokemon?pokemon=${search}`);
+  
         // put the jsonified data in state and set the loading state to false
 
-        const json = await response.json();
+    const json = await response.json();
 
-        setPokemon(json.results);
+    setPokemon(json);
+
+    setLoading(false);
   }
       
   return (
     <section className='pokemon'>
       {/* make the fetch on submit */}
-      <form>
+      <form onSubmit={handlePokemonSubmit}>
             Search pokemon for a city
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
         <button>Get pokemon</button>
