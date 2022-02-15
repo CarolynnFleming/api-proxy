66894123987ad257810e476809b48018a66f8616
CarolynnFleming <cfcatpink@gmail.com>
2022-02-14 20:10:45 -0500

    pokemon

--- src/PokemonSearch.js
+++ src/PokemonSearch.js
@@ -6,7 +6,7 @@ export default function PokemonSearch() {
       // you'll need to track your pokemon search results, the loading state, and one form field: name. For this form field, set a real initial values (like 'pikachu') so the form populates with a default value.
   const [loading, setLoading] = useState(false);
   const [pokemon, setPokemon] = useState([]);
-  const [search, setSearch] = useState('bulbasaur');
+  const [search, setSearch] = useState('pickachu');
 
   async function handlePokemonSubmit(e) {
     e.preventDefault();
@@ -14,7 +14,7 @@ export default function PokemonSearch() {
         // set the loading state to true
     setLoading(true);
         // use fetch to make a request to your netlify pokemon function. Be sure to pass the pokemon name as a query param in the URL
-    const response = await fetch(`/.netlify/functions/pokemon?pokemon=${search}`);
+    const response = await fetch(`/.netlify/functions/pokemon?search=${search}`);
   
         // put the jsonified data in state and set the loading state to false
 
@@ -31,14 +31,14 @@ export default function PokemonSearch() {
       <form onSubmit={handlePokemonSubmit}>
             Search pokemon for a city
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
-        <input onChange={e => setSearch(e.target.value)} value={search}/>
+        <input onChange={e => setSearch(e.target.value)} />
         <button>Get pokemon</button>
       </form>
       {/* Make a PokemonList component to import and use here. Use a ternery to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
       {
         loading 
           ? <img src={kodama} />
-          : <PokemonList pokemon={pokemon} />
+          : <PokemonList pokemon={pokemon} value={search}/>
       }
     </section>
   );
