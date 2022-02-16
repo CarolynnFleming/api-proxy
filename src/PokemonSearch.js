7705102fdf0895ea1309932a32c349df353692c8
CarolynnFleming <cfcatpink@gmail.com>
2022-02-15 20:09:03 -0500

    apikey/spinner/value

--- src/PokemonSearch.js
+++ src/PokemonSearch.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import PokemonList from './PokemonList';
-import kodama from './kodama.gif';
+import Spinner from './Spinner';
 
 export default function PokemonSearch() {
       // you'll need to track your pokemon search results, the loading state, and one form field: name. For this form field, set a real initial values (like 'pikachu') so the form populates with a default value.
@@ -31,14 +31,14 @@ export default function PokemonSearch() {
       <form onSubmit={handlePokemonSubmit}>
             Search pokemon for a city
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
-        <input onChange={e => setSearch(e.target.value)} />
+        <input value={search} onChange={e => setSearch(e.target.value)} />
         <button>Get pokemon</button>
       </form>
       {/* Make a PokemonList component to import and use here. Use a ternery to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
       {
         loading 
-          ? <img src={kodama} />
-          : <PokemonList pokemon={pokemon} value={search}/>
+          ? <Spinner />
+          : <PokemonList pokemon={pokemon} />
       }
     </section>
   );
